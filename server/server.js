cdb331f518a41de50c9c9eb43dd4de67d51a86e6
ubaishoodles <mohd.ubaish@oodles.io>
2024-03-20 01:53:38 +0530

    debugging serverless module issue

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080;
 
-// const mainRouter = require('./routes/main')
+const mainRouter = require('./routes/main')
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +18,7 @@ app.use(
     })
 );
 
-// app.use('/api/v1', mainRouter);
+app.use('/api/v1', mainRouter);
 
 app.get('/', (req, res) => {
     return res.status(200).json({ success: true, message: 'default route working fine!' });
