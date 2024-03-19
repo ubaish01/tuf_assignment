82ba99b124e6621de6a75dd85e2e45ec3b7c79b6
ubaishoodles <mohd.ubaish@oodles.io>
2024-03-19 13:41:23 +0530

    url update int constants

--- client/src/config/URL.ts
+++ client/src/config/URL.ts
@@ -1,5 +1,5 @@
-export const BACKEND_URL = 'http://localhost:8080'
-// export const BACKEND_URL = 'https://tuf-assignment-85e5yhyzj-ubaish01.vercel.app'
+// export const BACKEND_URL = 'http://localhost:8080'
+export const BACKEND_URL = 'https://tuf-assignment-85e5yhyzj-ubaish01.vercel.app'
 
 export const REQUEST = {
     DATA: '/data'
